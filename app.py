3a7706bf1e10b4e9921074db31174786a5989483
Prakash, Randheer <prrandheer621@gmail.com>
2024-08-17 23:31:52 +0530

    Update add code

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 task_list = []
 
 def generateRandomNumber():
-  num = time.time() * 1000
+  num = time.time_ns()
   return num
 
 def getTaskByID():
@@ -21,14 +21,14 @@ def putTaskById(title, derscription, ):
   
   return
 
-def addNewTask(title, description, created_at, updated_at, updated_by = 'guest'):
+def addNewTask(title, description, created_at, updated_by = 'guest'):
   task = {
     'id': generateRandomNumber(), #Math.random()
     'title': title, 
     'description': description, 
     'status': 'ACTIVE', #ACTIVE/INACTIVE
     'created_at': created_at, #DATETIME
-    'updated_at': updated_at, #DATETIME
+    'updated_at': created_at, #DATETIME
     'updated_by': updated_by
   }
 
@@ -46,15 +46,15 @@ def all_tasks():
     return jsonify(task_list)
 
   elif(request.method == 'POST'):
-    title = request.form['title']
-    description =  request.form['description']
-    created_at =  request.form['created_at']
-    updated_at =  request.form['updated_at']
-    updated_by =  request.form['updated_by']
+    body = request.json
+    title = body['title']
+    description =  body['description']
+    created_at =  time.time_ns()
+    # updated_at =  request.form['updated_at']
+    updated_by =  body['updated_by']
 
-    addNewTask(title, description, created_at, updated_at, updated_by)
-
-  return
+    addNewTask(title, description, created_at, updated_by)
+    return jsonify('Added successfully')
 
 @app.route('/tasks/<id>', methods=['GET', 'PUT', 'DELETE'])
 def task_by_id():
@@ -67,11 +67,6 @@ def task_by_id():
   elif(request.method == 'DELETE'):
     deleteTaskById()
 
-  return
-
-
-@app.route('/tasks/<id>')
-def task_by_id():
   return
 
 
