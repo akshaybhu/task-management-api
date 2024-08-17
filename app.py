b9aae04990dfa58e3c6854d9068296a3edb38354
“Akshay <abhutoria@salesforce.com>
2024-08-17 17:38:51 +0530

    updating app code

--- app.py
+++ app.py
@@ -1,27 +1,39 @@
-from flask import Flask, request, jsonify
-# from math import Math
-import random
-import time
+from flask import Flask, request, jsonify # type: ignore
+import random, time
 
 app = Flask(__name__)
 
 # id, title, description, status, created_at, updated_at
 
 task_list = []
+task_list_byID = []
 
 def generateRandomNumber():
   num = time.time() * 1000
-  return num
-
-def getTaskByID():
-  
+  return int(num)
 
 
-  return
+def getTaskByID(id):
+  for t in task_list:
+	  if t['id']== int(id):
+		  return t
 
-def putTaskById(title, derscription, ):
-  
-  return
+
+def putTaskById(id,title, description, updated_by):
+    # title = request.form['title']
+    # description =  request.form['description']
+    # updated_by =  request.form['updated_by']
+    
+    if int(id) in task_list_byID:
+      for t in task_list:
+        if int(id) == int(t['id']):
+          t[title] = title
+          t[description] = description
+          t[updated_by] = updated_by
+          return True
+    else:
+      return False
+
 
 def addNewTask(title, description, created_at, updated_at, updated_by = 'guest'):
   task = {
@@ -35,11 +47,19 @@ def addNewTask(title, description, created_at, updated_at, updated_by = 'guest')
   }
 
   task_list.append(task)
-  return
+  #print(task_list)
+  task_list_byID.append(task['id'])
 
-def deleteTaskById():
 
-  return
+def deleteTaskById(id):
+  if id in task_list_byID:
+    for t in task_list:
+      if id == t['id']:
+        task_list.remove(t)
+        task_list_byID.remove(id)
+        return True
+  else:
+     return False
 
 
 @app.route('/tasks', methods=['GET', 'POST'])
@@ -47,35 +67,47 @@ def all_tasks():
   if(request.method == 'GET'):
     return jsonify(task_list)
 
-  elif(request.method == 'POST'):
-    title = request.form['title']
-    description =  request.form['description']
-    created_at =  request.form['created_at']
-    updated_at =  request.form['updated_at']
-    updated_by =  request.form['updated_by']
+  else:
+   # (request.method == 'POST'):
+    body = request.json
+    title = body['title']
+    description =  body['description']
+    created_at =  ""
+    updated_at =  ""
+    updated_by =  ""
 
     addNewTask(title, description, created_at, updated_at, updated_by)
-
-  return
+    return "Added successfully"
 
 @app.route('/tasks/<id>', methods=['GET', 'PUT', 'DELETE'])
-def task_by_id():
+def task_by_id(id):
   if(request.method == 'GET'):
-    getTaskByID()
+    tsk = getTaskByID(id)
+    if tsk:
+      return jsonify(tsk)
+    else:
+       return "No such ID found"
+       
 
   elif(request.method == 'PUT'):
-    putTaskById()
+    body = request.json
+    title = body['title']
+    description =  body['description']
+    updated_by = body['updated_by']
+
+    updated_recall = putTaskById(id, title, description, updated_by)
+    if updated_recall:
+       return "Updated Successfully"
+    else:
+       return "No such ID found"
 
   elif(request.method == 'DELETE'):
-    deleteTaskById()
-
-  return
-
-
-@app.route('/tasks/<id>')
-def task_by_id():
-  return
+    updated_recall = deleteTaskById(id)
+    if updated_recall:
+      return "Updated Successfully"
+    else:
+      return "No such ID found"
 
 
 if __name__=='__main__':
-  app.run(debug=True)
+  app.run(debug=True, port=8001)
